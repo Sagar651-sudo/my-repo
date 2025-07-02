9d5756f09dbc09359125f704804d12c65c67176d
Sagar651-sudo <codedecode289@gmail.com>
2025-07-02 06:42:32 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   description = "The ID of the VPC"
   value       = aws_vpc.example_vpc.id
 }
+
+output "public_subnet_id" {
+  description = "The ID of the public subnet"
+  value       = aws_subnet.public_subnet.id
+}
