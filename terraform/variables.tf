d9b136dcaa355084d3fba847d90389e1ce1cf8f0
Sagar651-sudo <codedecode289@gmail.com>
2025-07-02 06:41:05 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,13 @@ variable "aws_region" {
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
+  description = "VPC CIDR block"
   type        = string
   default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "Public Subnet CIDR block"
+  type        = string
+  default     = "10.0.1.0/24"
 }
